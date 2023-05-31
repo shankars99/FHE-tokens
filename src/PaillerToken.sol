26e28b5583d9ec1cd6428983968ee5e0a01256f6
shankars99 <shankars0899@gmail.com>
2023-05-31 18:02:21 -0400

    Recv works

--- src/PaillerToken.sol
+++ src/PaillerToken.sol
@@ -6,19 +6,71 @@ import "lib/solmate/src/tokens/ERC20.sol";
 contract PaillerToken is ERC20 {
     address payable public owner;
 
+    event Deposit(address indexed from, uint256 amount);
+    event Withdrawal(address indexed to, uint256 amount);
+    event RecvNewTx(Tx new_tx);
+
+    struct Tx {
+        uint8 id;
+        address from;
+        bytes32 to;
+        bytes32 sharedKey;
+        bytes32 amount;
+    }
+    struct Confirmedblocks {
+        Tx[] txs;
+    }
+
+    uint8 public last_tx_id = 0;
+    uint256 public fees = 0 ether;
+
+    Tx[] public mempool;
+    mapping(uint256 => Confirmedblocks) confirmedblocks;
+
     constructor(uint8 _decimals) ERC20("PaillerToken", "Pai", _decimals) {
         owner = payable(msg.sender);
     }
 
-    function mint(address _to, uint256 _amount) internal {
+    function recvTx(bytes32 _to, bytes32 _sharedKey, bytes32 _amount) payable public {
+        require(msg.value == fees, "PaillerToken: amount must be equal to fees");
+
+        last_tx_id += 1;
+
+        Tx memory recv_tx = Tx({
+            id: last_tx_id,
+            from: msg.sender,
+            to: _to,
+            sharedKey: _sharedKey,
+            amount: _amount
+        });
+
+        mempool.push(recv_tx);
+
+        emit RecvNewTx(recv_tx);
+    }
+
+    function deposit(address _to, uint256 _amount) internal {
         _mint(_to, _amount);
+
+        emit Deposit(_to, _amount);
+    }
+
+    function withdrawal(uint256 _amount) public {
+        _burn(msg.sender, _amount);
+
+        payable(msg.sender).transfer(_amount);
+
+        emit Withdrawal(msg.sender, _amount);
     }
 
     function balance(address _account) public view returns (uint256) {
         return balanceOf[_account];
     }
 
+    event ReveivedEther(address indexed from, uint256 amount);
     receive() external payable {
-        mint(msg.sender, msg.value);
+        emit ReveivedEther(msg.sender, msg.value);
+
+        deposit(msg.sender, msg.value);
     }
 }
--- test/PaillerToken.t.sol
+++ test/PaillerToken.t.sol
@@ -7,9 +7,9 @@ import "src/PaillerToken.sol";
 contract ERC20Test is Test {
     PaillerToken public paillerToken;
 
-    address payable public owner;
-    address payable alice = payable(makeAddr("alice"));
-    address payable bob = payable(makeAddr("bob"));
+    address public owner;
+    address alice = makeAddr("alice");
+    address bob = makeAddr("bob");
 
     function setUp() public {
         paillerToken = new PaillerToken(8);
@@ -18,13 +18,45 @@ contract ERC20Test is Test {
         deal(bob, 100 ether);
     }
 
-    function testMintTokens() public {
-        uint256 amount = 10 ether;
-
+    function testDeposit() public {
         vm.startPrank(alice);
+        uint256 amount = 10 ether;
 
         (bool sent, ) = address(paillerToken).call{value: amount}("");
 
         assertEq(paillerToken.balance(alice), amount);
+        vm.stopPrank();
+    }
+
+    function testRecvTx() public {
+        bytes32 to = keccak256(abi.encodePacked(bob));
+        bytes32 sharedKey = keccak256(abi.encodePacked(alice, bob));
+        bytes32 amount = bytes32(uint256(0.1 ether));
+
+        vm.startPrank(alice);
+        (bool sent, ) = address(paillerToken).call{value: 0}(
+            abi.encodeWithSignature(
+                "recvTx(bytes32,bytes32,bytes32)",
+                to,
+                sharedKey,
+                amount
+            )
+        );
+
+        (
+            uint8 _id,
+            address _from,
+            bytes32 _to,
+            bytes32 _sharedKey,
+            bytes32 _amount
+        ) = paillerToken.mempool(0);
+
+        assertEq(sent, true);
+        assertEq(_id, 1);
+        assertEq(_from, alice);
+        assertEq(_to, to);
+        assertEq(_sharedKey, sharedKey);
+        assertEq(_amount, amount);
+        vm.stopPrank();
     }
 }
